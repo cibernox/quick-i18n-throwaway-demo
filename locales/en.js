75e131b31489c7dbf73366d10ffe99d556a5aa3e
Miguel Camba <miguel.camba@gmail.com>
2021-06-07 00:04:17 +0200

    Initial commit

---
+++ locales/en.js
@@ -0,0 +1,5 @@
+export default {
+  "recent.aria": "Find recently viewed tides",
+  "menu": "Menu",
+  "foot": "{count} {count, plural, =1 {foot} other {feet}}",
+}
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { addMessages, init, getLocaleFromNavigator /*, register */ } from 'svelte-intl-precompile';
+  import en from '../../locales/en.js';
+	import { t } from 'svelte-intl-precompile'
+
+  // @ts-ignore
+  addMessages('en', en);
+  // register('es', () => import('../../locales/en.js')); <-- use this approach if you want locales to be load lazily
+
+  init({
+    fallbackLocale: 'en',
+    initialLocale: getLocaleFromNavigator()
+  });
+  let count = 0;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>{$t("foot", { values: { count: count } })}</p>
+<button type="button" on:click={() => count++}>Increment count</button>
+<button type="button" on:click={() => count--}>Increment count</button>
